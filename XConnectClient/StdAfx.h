3529e26cd287a0035255a743a904ad3395cc97e3
okt <okt@9b37493c-9926-470a-a324-ce8200df6612>
2013-03-20 00:53:45 +0000

    fix compilation on systems without MFC libs

--- XConnectClient/StdAfx.h
+++ XConnectClient/StdAfx.h
@@ -12,12 +12,15 @@
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
+#if 0
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
 #include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
+#endif // 0
+typedef unsigned UINT;
 
 extern UINT XC_CALL;
 
